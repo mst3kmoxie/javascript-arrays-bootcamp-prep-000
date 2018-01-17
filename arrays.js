f869880070097b27e770f93a61dc39926fc9b99f
Dawn Carroll <dsc254@gmail.com>
2018-01-17 15:27:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var adjustedArray = array.unshift(element);
+  return adjustedArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
